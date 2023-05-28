f2860ecf612ad1054518359890c6c1baf84cdeb9
Thiago-S-Sousa <128821560+Thiago-S-Sousa@users.noreply.github.com>
2023-05-28 10:30:21 -0300

    Atualizando conteúdo e autoria do componente

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -8,14 +8,20 @@ import { Component } from '@angular/core';
 export class ListarPensamentoComponent {
   listaPensamentos = [
     {
-      conteudo: 'Angular',
-      autoria: 'Thi',
+      conteudo: 'Os bilhões são feitos de centavos.',
+      autoria: 'Felippi Crevellari',
       modelo: 'modelo3',
     },
     {
-      conteudo: 'Comunicação entre componentes',
-      autoria: 'Angular',
+      conteudo: 'Faça da sua vida mais "Responsivo',
+      autoria: 'Luana Monteiro',
       modelo: 'modelo1',
+    },
+    {
+      conteudo:
+        'No longínquo ano de 1991, iniciou-se o desenvolvimento do que viria a se tornar a plataforma Java como uma parte do programa Green, que agrupava projetos que tinham como objetivo estabelecer a nova geração de softwares inteligentes para dispositivos eletrônicos, como televisores, videocassetes, torradeiras e demais utensílios, para que esses pudessem se comunicar com outros do mesmo tipo e também com computadores...',
+      autoria: 'Artigo Java Alura',
+      modelo: 'modelo2',
     },
   ];
 }
